d72639d7b72864fbce907f9f2248a81130838ff0
Amit Naik <154658171+Amitnaik003@users.noreply.github.com>
2025-07-08 08:15:29 +0530

    Update urls.py

--- recipyproject/urls.py
+++ recipyproject/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from vege.views import *
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
  
 urlpatterns = [
